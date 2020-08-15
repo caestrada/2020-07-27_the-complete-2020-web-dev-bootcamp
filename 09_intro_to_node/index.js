035181808040c358210c44ec590c2b064f48096a
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-14 21:42:19 -0400

    NodeJS

---
+++ 09_intro_to_node/index.js
@@ -0,0 +1,9 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+ 
+let mySuperheroName = superheroes.random();
+console.log(':::::', mySuperheroName);
+
+let mySupervillainName = supervillains.random();
+console.log(':::::', mySupervillainName);
