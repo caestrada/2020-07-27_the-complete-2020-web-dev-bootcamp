b5397f8c70eb85b53e830d4f16959815ed16a5b3
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-16 18:33:54 -0400

    Display api data

--- 12_weather-project/app.js
+++ 12_weather-project/app.js
@@ -3,7 +3,7 @@ const https = require('https');
 const app = express();
 
 app.get('/', (req, res) => {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Miami,Florida&appid=0b07f5c2bdc7fc94875e0cfb5009d19a';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Miami,Florida&appid=0b07f5c2bdc7fc94875e0cfb5009d19a&units=imperial';
     https.get(url, (response) => {
         console.log(response.statusCode);
 
@@ -11,11 +11,15 @@ app.get('/', (req, res) => {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
-            console.log(temp);
-            console.log(weatherDescription);
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            console.log(imageUrl);
+            res.write('<p>The weather is currently '+weatherDescription+'</p>');
+            res.write('<h1>The temperature in Miami is '+temp+' degress Celcius.</h1>');
+            res.write("<img src="+imageUrl+">");
+            res.send();
         });
     });
-    res.send('Server is up and running.');
 })
 
 
