6dc31b60aeb105acb618a226b1df2af6c25aaaad
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-17 07:37:42 -0400

    get data from form

--- 12_weather-project/app.js
+++ 12_weather-project/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Miami,Florida&appid=0b07f5c2bdc7fc94875e0cfb5009d19a&units=imperial';
+    res.sendFile(__dirname+'/index.html');
+})
+
+app.post('/', (req, res) => {
+    const query = req.body.cityName;
+    const apiKey = '0b07f5c2bdc7fc94875e0cfb5009d19a';
+    const units = 'imperial';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
     https.get(url, (response) => {
         console.log(response.statusCode);
 
@@ -15,7 +25,7 @@ app.get('/', (req, res) => {
             const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
             console.log(imageUrl);
             res.write('<p>The weather is currently '+weatherDescription+'</p>');
-            res.write('<h1>The temperature in Miami is '+temp+' degress Celcius.</h1>');
+            res.write('<h1>The temperature in '+query+' is '+temp+' degress Celcius.</h1>');
             res.write("<img src="+imageUrl+">");
             res.send();
         });
