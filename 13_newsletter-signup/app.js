e025789e342015b0c5643e0ee5502177d09d344c
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-17 09:05:25 -0400

    Setup app to render static files

--- 13_newsletter-signup/app.js
+++ 13_newsletter-signup/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
-// const https = require('https');
+const https = require('request');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/signup.html');
 })
-
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
