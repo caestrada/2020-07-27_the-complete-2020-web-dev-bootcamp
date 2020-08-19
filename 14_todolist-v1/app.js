70b96ea5dccbce5120cf5d65e5566b4dee56f5aa
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-18 20:16:51 -0400

    Check day of week

---
+++ 14_todolist-v1/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+app.get('/', function (req, res) {
+  const today = new Date();
+  const currentDay = today.getDay();
+  if (currentDay === 6 || currentDay === 0) {
+    res.send("<h1>Yay it's the weekend</h1>");
+  } else {
+    res.sendFile(__dirname + '/index.html');
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+})
