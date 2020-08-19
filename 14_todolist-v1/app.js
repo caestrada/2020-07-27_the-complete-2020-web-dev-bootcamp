50c4cb90fac3cfd817238b3463aa0846d4a4dfc1
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-19 08:01:39 -0400

    Add css

--- 14_todolist-v1/app.js
+++ 14_todolist-v1/app.js
@@ -9,6 +9,7 @@ app.engine('ejs', engines.ejs);
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 let items = [
   'Buy food',
