7a716e723c3c57290691983132e28aea0b706cc2
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-19 07:18:45 -0400

    Set ejs

--- 14_todolist-v1/app.js
+++ 14_todolist-v1/app.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const engines = require('consolidate');
+const path = require('path');
 
 const app = express()
+
+app.engine('ejs', engines.ejs);
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   const today = new Date();
   const currentDay = today.getDay();
+
+  let day = '';
   if (currentDay === 6 || currentDay === 0) {
-    res.send("<h1>Yay it's the weekend</h1>");
+    day = 'Weekend';
   } else {
-    res.sendFile(__dirname + '/index.html');
+    day = 'Weekday';
   }
+  switch (currentDay) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+    default:
+      console.log('Error: current day is equal to: '+currentDay);
+  }
+
+  res.render('index', {day});
 })
 
 app.listen(3000, () => {
