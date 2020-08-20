e032f2ee7244be050fa32aa5ac88877b4f96c0a2
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-20 08:31:32 -0400

    Compose post

--- 15_ejs-blog/app.js
+++ 15_ejs-blog/app.js
@@ -27,6 +27,15 @@ app.get('/contact', (req, res) => {
   res.render('contact', {content: contactContent});
 });
 
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+  console.log(req.body);
+  res.redirect('/compose');
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
