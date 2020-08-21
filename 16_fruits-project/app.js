7d6b9005d5a897d34652ceffd093455a7d0b492e
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-21 19:05:40 -0400

    Add validation

--- 16_fruits-project/app.js
+++ 16_fruits-project/app.js
@@ -3,13 +3,20 @@ mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true, 
 
 // Fruit
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, 'Please check your data entry, no name specified.'],
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+    },
     review: String,
 });
 const Fruit = mongoose.model('Fruit', fruitSchema);
 const fruit = new Fruit(
-    { name: 'Apple', rating: 7, review: "Pretty solid of a fruit."}
+    { name: 'Apple', rating: 10, review: "Pretty solid of a fruit."}
 );
 // fruit.save();
 Fruit.find((err, fruits) => {
