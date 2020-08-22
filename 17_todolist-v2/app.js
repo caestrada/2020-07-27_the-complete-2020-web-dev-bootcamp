b705b6cdbc5432e9be7fc72881a8cab8bf6544ad
Carlos Estrada <carlos.estrada@piikaaboo.com>
2020-08-22 12:24:01 -0400

    Add new items

--- 17_todolist-v2/app.js
+++ 17_todolist-v2/app.js
@@ -52,16 +52,13 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res){
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
 
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  res.redirect('/');
 });
 
 app.get("/work", function(req,res){
